75cab4c69637028cc547ccc4f19ca9c72802d429
kre8mymedia <kre8mymedia@gmail.com>
2023-04-02 23:30:16 -0500

    changed default HOST

--- src/lib/config/index.ts
+++ src/lib/config/index.ts
@@ -1,4 +1,4 @@
 export const HOST =
-  process.env.NEXT_PUBLIC_HOST || 'wss://dev-api.promptengineers.ai';
+  process.env.NEXT_PUBLIC_HOST || 'wss://api.promptengineers.ai';
 // export const HOST = 'ws://localhost:8000';
 export const API_KEY = process.env.NEXT_PUBLIC_PE_API_KEY;
