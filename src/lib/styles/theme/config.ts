610fe83d9d427b009b6bf01676952187fbe52e7b
kre8mymedia <kre8mymedia@gmail.com>
2023-04-03 23:26:35 -0500

    Default to dark mode

--- src/lib/styles/theme/config.ts
+++ src/lib/styles/theme/config.ts
@@ -1,6 +1,7 @@
 import type { ThemeConfig } from '@chakra-ui/react';
 
 export const config: ThemeConfig = {
-  initialColorMode: 'system',
+  initialColorMode: 'dark',
+  useSystemColorMode: true,
   disableTransitionOnChange: false,
 };
