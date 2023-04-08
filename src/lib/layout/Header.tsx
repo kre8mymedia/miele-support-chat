13d3684d18971ea4b5b06e96ea3f9434886fafda
kre8mymedia <kre8mymedia@gmail.com>
2023-04-07 23:08:45 -0500

    Small touch

--- src/lib/layout/Header.tsx
+++ src/lib/layout/Header.tsx
@@ -7,7 +7,7 @@ const Header = () => {
   return (
     <Flex as="header" width="full" align="center" py={2}>
       <Box>
-        <Link href="https://python.langchain.com/en/latest"><ChakraLink ml={5}>Langchain GPT</ChakraLink></Link> - v0.0.134
+        <ChakraLink ml={5} href="https://python.langchain.com/en/latest">Langchain GPT</ChakraLink> - v0.0.134
         <Text fontSize="xs" ml={5} colorScheme="blue">
           <Link
             href="https://promptengineers.ai"
